1f1a5cc3e33011d4f2ad9a97168bd508cd8db7af
Ami <aryal.rajat05@gmail.com>
2024-05-26 17:30:42 +0545

    Brand page of shoes

---
+++ Brand-Page/src/components/Navigation.jsx
@@ -0,0 +1,31 @@
+
+
+
+const Navigation =()=>{
+
+     return(
+          <div>
+          <nav className="container">
+          <div className='logo'>
+            <img src="images\brand_logo.jpg" alt="" />
+          </div>
+    
+          <ul>
+            <li href="#">Home</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+          </ul>
+    
+          <button className='btn'>Login</button>
+        </nav>
+
+
+
+
+          </div>
+     )
+
+ }
+
+ export default Navigation;
---
+++ Brand-Page/src/components/Section.jsx
@@ -0,0 +1,31 @@
+const Section = ()=>{
+     return(
+         <main className="section container">
+          <div className="section-content">
+               <h1>
+                    YOUR FEET DESERVE THE BEST
+               </h1>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolore necessitatibus. Omnis corporis aut
+                perferendis alias nemo a? Deleniti, nemo!</p>
+                <div className="section-btn">
+               <button className="btn">Shop Now</button>
+               <button className="secondary-btn">Category</button>
+               </div>
+          <div className="shopping">
+               <p>Availabe only on</p>
+          </div>
+          <div className="brand-icons">
+               <img src="/images/amazon.png" alt="amazon logo" />
+               <img src="/images/flipkart.png" alt="amazon logo" />
+          </div>
+
+          
+          </div>
+         
+          <div className="section-img">
+               <img src="/images/shoe.png" alt="" />
+          </div>
+         </main>
+     )
+}
+export default Section;
