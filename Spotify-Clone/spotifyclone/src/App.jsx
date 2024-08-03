e27a899cc03ecd78bf67156b44dd3a5fbb1abeb9
aryal05 <aryal.rajat05@gmail.com>
2024-08-03 20:21:19 +0545

    first commit

---
+++ Spotify-Clone/spotifyclone/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SideBar from './components/SideBar'
+
+const App = () => {
+  return (
+    <>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <SideBar/>
+      </div>
+    
+      </div>
+    </>
+  )
+}
+
+export default App
