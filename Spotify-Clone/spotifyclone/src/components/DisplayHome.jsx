137b7d05c7f1f6b2184fb45947310e15ab236fd1
aryal05 <aryal.rajat05@gmail.com>
2024-08-04 18:30:23 +0545

    featured chart

--- Spotify-Clone/spotifyclone/src/components/DisplayHome.jsx
+++ Spotify-Clone/spotifyclone/src/components/DisplayHome.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import NavBar from './NavBar'
+import {albumsData} from '../assets/assets'
+import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
   return (
     <>
     <NavBar/>
+    <div className='mb-4'>
+      <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+      <div className='flex overflow-auto'>
+      {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+
+      </div>
+    
+
+
+    </div>
     </>
   )
 }
