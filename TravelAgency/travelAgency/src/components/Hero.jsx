6b9a4eeab8fb3a298bef844c2378233b36cd2c3e
aryal05 <aryal.rajat05@gmail.com>
2024-08-17 21:32:35 +0545

    heo section completion

--- TravelAgency/travelAgency/src/components/Hero.jsx
+++ TravelAgency/travelAgency/src/components/Hero.jsx
@@ -5,17 +5,18 @@ const Hero = ({hero:{title,subtitle,btn1, btn2, text, img}}) => {
    <>
    <div className='flex flex-col bg-gradient-to-b from-emerald-200 to-white h-auto w-auto'> 
     <div className='travigo-container grid items-start justify-items-center'>
-        <div className='grid items-center justify-center mt-36 mb-16 md:mt-28 md:mb-12 '>
+        <div className='grid items-center justify-items-center mt-36 mb-16 md:mt-28 md:mb-12 '>
             <h1 className='text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg'>{title}</h1>
             <h1 className='text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg'>{subtitle}</h1>
-            <p className=''>{text}</p>
-            <div className=''>
-                <button type='button' className=''>{btn1}</button>
-                <button type='button' className=''>{btn2}</button>
+            <p className='text-base my-5 text-center sm:text-sm'>{text}</p>
+            <div className='flex items-center justify-center gap-11 sm:gap-3 sm:flex-col sm:w-full'>
+                <button type='button' className='button-emrald'>{btn1}</button>
+                <button type='button' className='button-light'>{btn2}</button>
             </div>
         </div>
-        <div className=''>
-            <img src={img} alt='dashboard/image' className=''/>
+        <div className='flex items-center justify-center mb-11'>
+            <img src={img} alt='dashboard/image' className='h-[85vh] w-full object-fill lg:h-[75vh]
+            md:h-[57vh] sm:h-[35vh] sm:object-contain drop-shadow-emrald'/>
         </div>
     </div>
    </div>
