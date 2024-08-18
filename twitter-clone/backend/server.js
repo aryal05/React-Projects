fe45e1053357b3d73be357b0f0723525edac934b
aryal05 <aryal.rajat05@gmail.com>
2024-08-18 10:42:35 +0545

    backend file setup

---
+++ twitter-clone/backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import authRoutes from './routes/auth.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+console.log(process.env.MONGO_URI)
+
+app.use("/api/auth",authRoutes)
+
+app.listen(8000,()=>{
+    console.log("Server is running on port 8000")
+});
