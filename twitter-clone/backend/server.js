310891fa93db0b00a8f71c3bce4d78925e902aca
aryal05 <aryal.rajat05@gmail.com>
2024-08-18 11:41:47 +0545

    mongodb setup

--- twitter-clone/backend/server.js
+++ twitter-clone/backend/server.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import authRoutes from './routes/auth.js'
 import dotenv from 'dotenv'
+import connectMongoDb from './db/connectMongoDb.js'
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI)
+// console.log(process.env.MONGO_URI)
 
 app.use("/api/auth",authRoutes)
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000")
+app.listen(PORT,()=>{
+    console.log(`Serrver is running on port ${PORT}`)
+    connectMongoDb();
 });
