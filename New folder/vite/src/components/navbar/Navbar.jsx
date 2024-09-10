309f2ed380855be20500c7501493d448f34e0554
aryal05 <aryal.rajat05@gmail.com>
2024-09-10 22:03:13 +0545

    change in color

--- New folder/vite/src/components/navbar/Navbar.jsx
+++ New folder/vite/src/components/navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-       <div className='flex items-center justify-around bg-gray-50 space-x-6 p-3'>
-        <div className='mr-3'>
+       <div className='flex items-center justify-around bg-black space-x-6 p-3'>
+        <div className='mr-3 text-gray-300'>
             <h1>Logo Here</h1>
         </div>
    
-      <div className='space-x-9'>
+      <div className='space-x-9 text-gray-300'>
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
       <div className=''>
         <input type="text" className='h-10 border-2 rounded-md border-teal-600' name="" id="" placeholder='Seach Here'/>
-            <button>Search</button>
+            <button className='bg-lime-600 border-2 border-teal-700 ml-3 rounded-md w-16 text-white'>Search</button>
         </div>
  
     </div>
