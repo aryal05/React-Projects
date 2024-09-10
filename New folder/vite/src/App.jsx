7bb360595d2c59e09b465c08fc4286ddc53ed448
aryal05 <aryal.rajat05@gmail.com>
2024-09-10 20:29:48 +0545

    router setrup

--- New folder/vite/src/App.jsx
+++ New folder/vite/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-   <>
-   App
-   
-   </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
   )
 }
 
