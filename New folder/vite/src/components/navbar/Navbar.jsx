c5cca652b6671d18b67f7f7bb34faab5e369ea69
aryal05 <aryal.rajat05@gmail.com>
2024-09-10 22:08:53 +0545

    hover effect

--- New folder/vite/src/components/navbar/Navbar.jsx
+++ New folder/vite/src/components/navbar/Navbar.jsx
@@ -7,13 +7,28 @@ const Navbar = () => {
         <div className='mr-3 text-gray-300'>
             <h1>Logo Here</h1>
         </div>
-   
-      <div className='space-x-9 text-gray-300'>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+ 
+        <div className='space-x-9 text-gray-300'>
+  <a 
+    href="/" 
+    className="border-2 border-transparent hover:border-gray-400  hover:bg-slate-500  transition-colors duration-300 rounded-md"
+  >
+    Home
+  </a>
+  <a 
+    href="/about" 
+    className="border-2 border-transparent hover:border-gray-400 hover:bg-slate-500 transition-colors duration-300 rounded-md"
+  >
+    About
+  </a>
+  <a 
+    href="/contact" 
+    className="border-2 border-transparent hover:border-gray-400  hover:bg-slate-500 transition-colors duration-300 rounded-md"
+  >
+    Contact
+  </a>
+</div>
 
-      </div>
       <div className=''>
         <input type="text" className='h-10 border-2 rounded-md border-teal-600' name="" id="" placeholder='Seach Here'/>
             <button className='bg-lime-600 border-2 border-teal-700 ml-3 rounded-md w-16 text-white'>Search</button>
