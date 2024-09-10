af1eb85403e2ccdc516f0d05cb5263d77933e4a7
aryal05 <aryal.rajat05@gmail.com>
2024-09-10 21:14:21 +0545

    search but

--- New folder/vite/src/components/navbar/Navbar.jsx
+++ New folder/vite/src/components/navbar/Navbar.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-       <div className='flex items-center justify-around bg-gray-50 '>
-        <div>
+       <div className='flex items-center justify-around bg-gray-50 space-x-6 p-3'>
+        <div className='mr-3'>
             <h1>Logo Here</h1>
         </div>
    
-      <div className='space-x-3 px-3 p-3 mr-3'>
+      <div className='space-x-9'>
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
 
       </div>
+      <div className=''>
+            <button>Search</button>
+        </div>
  
     </div>
     </>
