98470fdc9185c119fafc4ecc131c5482458c317d
aryal05 <aryal.rajat05@gmail.com>
2024-09-11 20:27:05 +0545

    Find Yopur stayt div

--- New folder/vite/src/components/home/Home.jsx
+++ New folder/vite/src/components/home/Home.jsx
@@ -8,9 +8,17 @@ const Home = () => {
           className="w-screen h-screen bg-no-repeat bg-cover bg-center"
           style={{ backgroundImage: 'url("/images/head.jpeg")' }}
         >
-          <h1 className='text-white font-bold hover:text-teal-400 hover:underline cursor-pointer inline-block'>
-            Welcome here
+          <h1 className='text-white font-bold hover:text-teal-400 hover:underline cursor-pointer inline-block duration-300'>
+            Welcome 
           </h1>
+          <div className='flex justify-center items-center h-screen'>
+  <div className="w-72 h-24 flex justify-center items-center font-bold bg-orange-200 text-center rounded-full transition duration-300 hover:bg-orange-400 cursor-pointer">
+    <h1>Find Your Stay</h1>
+  </div>
+</div>
+
+
+
         </div>
         <div>
           {/* Additional content here */}
