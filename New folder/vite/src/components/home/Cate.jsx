07dbd0d172963b1325e1de5d1f8c6db34fe9e482
aryal05 <aryal.rajat05@gmail.com>
2024-09-12 19:58:06 +0545

    cate section

--- New folder/vite/src/components/home/Cate.jsx
+++ New folder/vite/src/components/home/Cate.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
 
+
+
 const Cate = () => {
   return (
     <>
-    <div className=''>
-        <h1 className='m-2 p-3 text-2xl font-extralight underline inline-block'>Categories</h1>       
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-7">
         <div>
-            <img src="200x300" alt="" />
-            <p>Single and Double </p>
-
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
         </div>
-    </div>
-
-
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="" />
+        </div>
+        <div>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Cate
