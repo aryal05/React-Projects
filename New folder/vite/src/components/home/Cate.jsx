14d811fe4d9c6a1c8cdcad7fa667fca0a789e9c7
aryal05 <aryal.rajat05@gmail.com>
2024-09-12 20:10:38 +0545

    Cate section done

--- New folder/vite/src/components/home/Cate.jsx
+++ New folder/vite/src/components/home/Cate.jsx
@@ -1,52 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-
+const categories = [
+  { id: 1, name: "Hair Gel", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" },
+  { id: 2, name: "Shampoo", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" },
+  { id: 3, name: "Conditioner", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" },
+  { id: 4, name: "Hair Spray", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" },
+  { id: 5, name: "Comb", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" },
+  { id: 6, name: "Hair Brush", imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" },
+];
 
 const Cate = () => {
   return (
     <>
       <h1 className='mt-5 font-extralight text-xl flex ml-4 underline'>Categories</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-7">
-      
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="" />
-        </div>
-        <div>
-          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="" />
-        </div>
+        {categories.map((category) => (
+          <div key={category.id} className="bg-white rounded-lg shadow-md p-4">
+            <img className="h-auto max-w-full rounded-lg" src={category.imageUrl} alt={category.name} />
+            <h1 className='flex items-center justify-center font-extralight text-xl mt-4'>{category.name}</h1>
+          </div>
+        ))}
       </div>
     </>
   );
 }
 
-export default Cate
+export default Cate;
