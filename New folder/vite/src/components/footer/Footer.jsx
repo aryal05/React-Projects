c207c5a1420517df9183169c42072314aefe5658
aryal05 <aryal.rajat05@gmail.com>
2024-09-14 21:40:16 +0545

    lasst commit

--- New folder/vite/src/components/footer/Footer.jsx
+++ New folder/vite/src/components/footer/Footer.jsx
@@ -1,52 +1,35 @@
 import React from 'react'
 
 const Footer = () => {
+  const regretsData = [
+    { id: 1, title: "Regrets of Anusha...", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 2, title: "Regrets of Anusha..", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 3, title: "Regrets of Anusha..", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] }
+  ];
+
   return (
     <>
-   <div className='bg-slate-600 w-full flex justify-center'>
-    <div>
-        <h1 className='font-extralight text-xl text-yellow-400 text-center'>Types of Regrets</h1>
-        <div class="flex justify-around space-x-12">
-
-    <div class="p-4">
-        <h1 class="text-xl font-bold">Regrets of:...</h1>
-        <ul class="mt-4">
-            <li><a href="#">No:1</a></li>
-            <li><a href="#">No:2</a></li>
-            <li><a href="#">No:3</a></li>
-            <li><a href="#">No:4</a></li>
-            <li><a href="#">No:5</a></li>
-        </ul>
-    </div>
-
- 
-    <div class="p-4 ">
-        <h1 class="text-xl font-bold">Regrets of:...</h1>
-        <ul class="mt-4">
-            <li><a href="#">No:1</a></li>
-            <li><a href="#">No:2</a></li>
-            <li><a href="#">No:3</a></li>
-            <li><a href="#">No:4</a></li>
-            <li><a href="#">No:5</a></li>
-        </ul>
-    </div>
-
-    <div class="p-4">
-        <h1 class="text-xl font-bold">Regrets of:...</h1>
-        <ul class="mt-4">
-            <li><a href="#">No:1</a></li>
-            <li><a href="#">No:2</a></li>
-            <li><a href="#">No:3</a></li>
-            <li><a href="#">No:4</a></li>
-            <li><a href="#">No:5</a></li>
-        </ul>
-    </div>
-</div>
-
-    </div>
-   </div>
+      <div className='bg-slate-600 w-full flex justify-center'>
+        <div>
+          <h1 className='font-extralight text-xl text-yellow-400 text-center'>Types of Regrets</h1>
+          <div className="flex justify-around space-x-12">
+            {regretsData.map((regretSection) => (
+              <div key={regretSection.id} className="p-4">
+                <h1 className="text-xl font-bold">{regretSection.title}</h1>
+                <ul className="mt-4">
+                  {regretSection.regrets.map((regret, index) => (
+                    <li key={index}>
+                      <a href="#">{regret}</a>
+                    </li>
+                  ))}
+                </ul> 
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
