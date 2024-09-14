613901817a64abb42121cc438c92fa18d0285080
aryal05 <aryal.rajat05@gmail.com>
2024-09-14 20:50:49 +0545

    listing

--- New folder/vite/src/components/footer/Footer.jsx
+++ New folder/vite/src/components/footer/Footer.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-   <div className='bg-slate-600 w-full'>
-    <div className='flex items-center justify-center'>
-        <h1 className='font-extralight text-xl '>Regrets Of Anusha</h1>
+   <div className='bg-slate-600 w-full flex items-center justify-center'>
+    <div className=''>
+        <h1 className='font-extralight text-xl text-yellow-400'>Regrets Of Anusha</h1>
         <div>
             <div>
+                <ul>
+                    <li><a href='#'>No:1</a></li>
+                    <li><a href='#'>No:2</a></li>
+                    <li><a href='#'>No:3</a></li>
+                    <li><a href='#'>No:4</a></li>
+                    <li><a href='#'>No:5</a></li>
+                </ul>
 
             </div>
         </div>
