83bf0dbd659d3333a3f6015a5bfcfcae853c5f0b
aryal05 <aryal.rajat05@gmail.com>
2024-09-15 19:40:58 +0545

    C:/Program Files/Git/about

--- New folder/vite/src/components/about/About.jsx
+++ New folder/vite/src/components/about/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
-  )
+    <>
+      <div className="">
+        <img className='m-7 p-7' src="/images/about.jpeg" alt="About" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="text-white text-3xl font-bold bg-black bg-opacity-50 p-4 rounded">
+          About Us
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
