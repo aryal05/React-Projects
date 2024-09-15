a9a78f6153ec2d88df3ae0151f8ac6991c1d1554
aryal05 <aryal.rajat05@gmail.com>
2024-09-15 19:33:25 +0545

    fotter updation

--- New folder/vite/src/components/footer/Footer.jsx
+++ New folder/vite/src/components/footer/Footer.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 const Footer = () => {
   const regretsData = [
-    { id: 1, title: "Regrets of Anusha...", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
-    { id: 2, title: "Regrets of Anusha..", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
-    { id: 3, title: "Regrets of Anusha..", regrets: ["No:1", "No:2", "No:3", "No:4", "No:5"] }
+    { id: 1, title: "info", infos: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 1, title: "info", infos: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 1, title: "info", infos: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 1, title: "info", infos: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
+    { id: 1, title: "info", infos: ["No:1", "No:2", "No:3", "No:4", "No:5"] },
   ];
 
   return (
     <>
       <div className='bg-slate-600 w-full flex justify-center'>
         <div>
-          <h1 className='font-extralight text-xl text-yellow-400 text-center'>Types of Regrets</h1>
+          <h1 className='font-extralight text-xl text-yellow-400 text-center'>Types of Infos</h1>
           <div className="flex justify-around space-x-12">
             {regretsData.map((regretSection) => (
               <div key={regretSection.id} className="p-4">
                 <h1 className="text-xl font-bold">{regretSection.title}</h1>
                 <ul className="mt-4">
-                  {regretSection.regrets.map((regret, index) => (
+                  {regretSection.infos.map((regret, index) => (
                     <li key={index}>
                       <a href="#">{regret}</a>
                     </li>
