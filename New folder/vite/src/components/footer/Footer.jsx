f23b3d94700479e42839760e585b3e3681a9e86a
aryal05 <aryal.rajat05@gmail.com>
2024-09-17 21:02:27 +0545

    home and footer

--- New folder/vite/src/components/footer/Footer.jsx
+++ New folder/vite/src/components/footer/Footer.jsx
@@ -16,12 +16,15 @@ const Footer = () => {
           <div className="flex justify-around space-x-12">
             {Data.map((Section) => (
               <div key={Section.id} className="p-4 mt-6">
+                
                 <h1 className="text-xl font-bold">{Section.title}</h1>
+                
                 <ul className="mt-4 text-white font-extralight leading-9">
+                  
                   {Section.infos.map((regret, index) => (
                     <li key={index}>
                       <a href="#">{regret}</a>
-                    </li>
+                    </li> 
                   ))}
                 </ul> 
               </div>
