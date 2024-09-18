f81c4bcf96bb5b395a5fd1f0aa59875cb8088c26
aryal05 <aryal.rajat05@gmail.com>
2024-09-18 20:36:20 +0545

    shoop section heading p and button

--- New folder/vite/src/shop/Shop.jsx
+++ New folder/vite/src/shop/Shop.jsx
@@ -6,7 +6,13 @@ const Shop = () => {
 <div>
     <div className='flex justify-between'>
         <div className='items-center '>
-        <h1 className=''>Welcome to our shop page </h1>
+        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold leading-tight text-red-600 m-10 p-10 '>Find your Products  </h1>
+        <p className="text-slate-500 w-[320px] text-xl ml-20">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione harum dolores consequuntur magni. Nesciunt ratione optio maxime!
+              </p>
+              <button className="bg-red-600 text-white  ml-20 border-2 border-red-600 py-2 px-5 mt-6 lg:mt-8 text-lg rounded-md hover:bg-transparent hover:text-red-600 hover:border-red-600 transition-colors duration-300">
+                Shop
+              </button>
         </div>
         <div>
             <img src="images/shoes.jpg" className='w-[800px] justify-center mt-10 rounded-full' alt="" />
