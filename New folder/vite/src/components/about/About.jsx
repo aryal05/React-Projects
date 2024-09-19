34732b27ead24840ceb019be16988b9b24a01dd3
aryal05 <aryal.rajat05@gmail.com>
2024-09-19 20:50:54 +0545

    about section changes

--- New folder/vite/src/components/about/About.jsx
+++ New folder/vite/src/components/about/About.jsx
@@ -5,13 +5,20 @@ import Items from './Items';
 const About = () => {
   return (
     <><div className='bg-red-600 text-white h-24 flex items-center justify-center text-4xl font-bold'>
-      <h1>About Us</h1>
+      <h1 className='font-bold text-5xl'>About Us</h1>
     </div>
-      <div className="relative">
+      <div className="relative flex items-center justify-center mt-9">
         {/* <img className='m-7 p-7' src="/images/about.jpeg" alt="About" /> */}
-        <h1>Why Shop With Us</h1>
+        <h1 className='font-bold text-4xl'>Why Shop With Us</h1>
+      
      
       </div>
+      <div className='flex items-center justify-center'>
+          <div className=' border-2 border-bg-gray-800'>
+            <h1>Free Delivery</h1>
+            <p>Here We provide free Delivery</p>
+          </div>
+        </div>
       {/* <hr /> */}
 <Items/>
 
