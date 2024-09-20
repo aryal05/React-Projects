f370848257c6f29e863bfa11747a7379146e0375
aryal05 <aryal.rajat05@gmail.com>
2024-09-20 19:10:31 +0545

    bg in contatc

--- New folder/vite/src/components/contact/Contact.jsx
+++ New folder/vite/src/components/contact/Contact.jsx
@@ -4,8 +4,8 @@ import Form from './Form'
 const Contact = () => {
   return (
     <>
-    <div className='flex items-center justify-center bg-red-600 text-white'>
-      <div className=''>
+    <div className='flex items-center justify-center bg-red-600 text-white h-24'>
+      <div className='text-4xl font-bold'>
         <div>
           <h1>Connect With US</h1>
         </div>
