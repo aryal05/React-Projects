a180ffcb5a2c0b8e0ba84b92d93346115e9e352b
aryal05 <aryal.rajat05@gmail.com>
2024-09-20 19:08:40 +0545

    conatct section

--- New folder/vite/src/components/contact/Contact.jsx
+++ New folder/vite/src/components/contact/Contact.jsx
@@ -3,10 +3,18 @@ import Form from './Form'
 
 const Contact = () => {
   return (
-    <div>
-
-      <Form/>
+    <>
+    <div className='flex items-center justify-center bg-red-600 text-white'>
+      <div className=''>
+        <div>
+          <h1>Connect With US</h1>
+        </div>
+      </div>
     </div>
+
+
+<Form/>
+    </>
   )
 }
 
