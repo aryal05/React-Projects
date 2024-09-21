f5c5bce9e645604563bd2f956a1643466f7ee909
aryal05 <aryal.rajat05@gmail.com>
2024-09-21 19:54:57 +0545

    para updation in post

--- New folder/vite/src/components/contact/Post.jsx
+++ New folder/vite/src/components/contact/Post.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div className='flex justify-around border-2'>
-      <div className=''>
+    <div className='flex justify-around items-center border-2'>
+      <div className='item-center'>
       <h1>Post Title</h1>
-        <p>Explore the items you need with us with more than 5 years of trust from the people and relaible  service</p>.
+        <p className='w-72'>Explore the items you need with us with more than 5 years of trust from the people and relaible  service.
+           
+Explore the items you need with us, backed by over 5 years of trust from our valued customers and reliable service. We offer a diverse range of high-quality products designed to meet your every need. With a commitment to excellence, we ensure fast delivery, exceptional customer support, and a seamless shopping experience that you can rely on, time and time again.
+ Join our community of satisfied shoppers and discover why we are a trusted name in the industry. </p>.
 
       </div>
       <div>
