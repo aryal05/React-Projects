ad4a9ba75dd78166ff4323c4046afc467be0b859
aryal05 <aryal.rajat05@gmail.com>
2024-09-21 19:50:39 +0545

    post section startup

--- New folder/vite/src/components/contact/Post.jsx
+++ New folder/vite/src/components/contact/Post.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div>
-      
+    <div className='flex justify-around border-2'>
+      <div className=''>
+        <h1>Post Title</h1>
+        <p>Explore the items you need with us with more than 5 years of trust from the people and relaible  service</p>.
+
+      </div>
+      <div>
+        <img src="images/bg.avif" alt="" />
+      </div>
     </div>
   )
 }
