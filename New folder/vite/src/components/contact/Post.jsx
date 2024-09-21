8e9a3c8b640d47e3b6a3303e3ec701ff61ed438b
aryal05 <aryal.rajat05@gmail.com>
2024-09-21 20:42:03 +0545

    para text lg

--- New folder/vite/src/components/contact/Post.jsx
+++ New folder/vite/src/components/contact/Post.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Post = () => {
   return (
     <div className='flex justify-around items-center border-2 bg-cyan-950 rounded-xl text-white'>
-      <div className='item-center border-2 border-slate-600'>
+      <div className='item-center'>
       <h1 className='text-3xl font-bold mb-4 underline decoration-slate-600'>Reason Behind</h1>
-        <p className='w-80'>Explore the items you need with us with more than 5 years of trust from the people and relaible  service.
+        <p className='w-80 font-extralight text-lg'>Explore the items you need with us with more than 5 years of trust from the people and relaible  service.
            
 Explore the items you need with us, backed by over 5 years of trust from our valued customers and reliable service. We offer a diverse range of high-quality products designed to meet your every need. With a commitment to excellence, we ensure fast delivery, exceptional customer support, and a seamless shopping experience that you can rely on, time and time again.
  Join our community of satisfied shoppers and discover why we are a trusted name in the industry. </p>
