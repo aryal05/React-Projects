507221f7a2230ec4e2b2867f16eeba80d40d5ee4
aryal05 <aryal.rajat05@gmail.com>
2024-09-23 21:02:59 +0545

    company name and date

--- New folder/vite/src/components/footer/Footer.jsx
+++ New folder/vite/src/components/footer/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
                 </a>
               </p>
               <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
-                &copy; 2022 Company Name
+                &copy; 2024 Rajat Branding
               </p>
             </div>
           </div>
