f07705646bea26d4fc6a9f8c9ca279cabc6a18e3
aryal05 <aryal.rajat05@gmail.com>
2024-10-14 19:22:16 +0545

    products datta  added

--- New folder/vite/src/components/about/Items.jsx
+++ New folder/vite/src/components/about/Items.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Items = () => {
+  const [cart, setCart] = useState([]);
+   // Products data
+   const products = [
+    { name: 'Product 1', description: 'Product description excerpt...', price: 5.05 },
+    { name: 'Product 2', description: 'Product description excerpt...', price: 8.50 },
+    { name: 'Product 3', description: 'Product description excerpt...', price: 10.50 }
+  ];
   return (
-    <div>
-      <h1>  Items</h1>
-    </div>
+  <>
+
+  
+  
+  </>
   )
 }
 
