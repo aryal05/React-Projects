570ea0032b551ae2fd572427653c0060fd5d470f
aryal05 <aryal.rajat05@gmail.com>
2024-10-16 19:45:49 +0545

    chyange in color textor

--- New folder/vite/src/components/contact/Service.jsx
+++ New folder/vite/src/components/contact/Service.jsx
@@ -3,27 +3,27 @@ import React from 'react'
 const Service = () => {
   return (
    <>
-     <section class="bg-cyan-950 py-20">
+     <section class="py-20 bg-red-400 text-white">
   <div class="container mx-auto p-4">
     <h2 class="text-3xl font-bold mb-4">Our Services</h2>
-    <div class="flex flex-wrap -mx-4">
+    <div class="flex flex-wrap mx-4">
       <div class="w-full md:w-1/2 xl:w-1/3 p-4">
-        <div class="bg-white shadow-md p-4 rounded">
-          <i class="fas fa-truck text-4xl text-blue-500 mb-4"></i>
+        <div class="bg-white shadow-md p-4 rounded  hover:bg-slate-400 bg-opacity-5">
+          <i class="fas fa-truck text-4xl"></i>
           <h3 class="text-2xl font-bold mb-2">Fast Shipping</h3>
           <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
         </div>
       </div>
       <div class="w-full md:w-1/2 xl:w-1/3 p-4">
         <div class="bg-white shadow-md p-4 rounded">
-          <i class="fas fa-lock text-4xl text-blue-500 mb-4"></i>
+          <i class="fas fa-lock text-4xl"></i>
           <h3 class="text-2xl font-bold mb-2">Secure Payment</h3>
           <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
         </div>
       </div>
       <div class="w-full md:w-1/2 xl:w-1/3 p-4">
         <div class="bg-white shadow-md p-4 rounded">
-          <i class="fas fa-headphones text-4xl text-blue-500 mb-4"></i>
+          <i class="fas fa-headphones text-4xl"></i>
           <h3 class="text-2xl font-bold mb-2">24/7 Support</h3>
           <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
         </div>
