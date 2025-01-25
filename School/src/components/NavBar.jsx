ceb198024b37ad9e930f312057b6780a256a12b3
aryal05 <aryal.rajat05@gmail.com>
2025-01-25 19:15:10 +0545

    navlinks created

--- School/src/components/NavBar.jsx
+++ School/src/components/NavBar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between py-5 font-medium'>
+      {/* <img src="" alt="" /> */}
+
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to='/' className= 'flex flex-col items-center gap-1'>
+        <p>HOME</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        <NavLink to='/blog' className= 'flex flex-col items-center gap-1'>
+        <p>BLOG</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        <NavLink to='/about' className= 'flex flex-col items-center gap-1'>
+        <p>ABOUT</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+
+        <NavLink to='/contact' className= 'flex flex-col items-center gap-1'>
+        <p>CONTACT</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        <NavLink to='/login' className= 'flex flex-col items-center gap-1'>
+        <p>LOGIN</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+        </NavLink>
+        
+      </ul>
     </div>
   )
 }
